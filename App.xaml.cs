b0f8bc36f2f8e6bb1e35dde9efc2c2bb9cee7e22
BitTim <tim.anhalt.2004@protonmail.com>
2021-07-20 16:02:31 +0200

    [Vex-61]: Added Color Gradients

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace VexTrack
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
+		
 	}
 }

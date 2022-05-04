495c6707e08c77c6aebfd804ad9f6763ff5dc586
TIm Anhalt <tim@anhalt.me>
2022-05-04 19:05:35 +0200

    [VEX-102]: Updated to not include API Keys in git repo
    Old keys have been removed

--- Web/import-tool/src/firebase/index.ts
+++ Web/import-tool/src/firebase/index.ts
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyA5Tdvil2nOc_97z_6ANk7bXITSAcvr7ko",
-	authDomain: "vextrack-c530b.firebaseapp.com",
-	projectId: "vextrack-c530b",
-	storageBucket: "vextrack-c530b.appspot.com",
-	messagingSenderId: "975706202791",
-	appId: "1:975706202791:web:6552401bb9a333b78af182",
-	measurementId: "G-JT8KXNWF7Y"
-  };
+import firebaseConfig from "./config";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

03be685661742aa5df9a55e37146741994f4ecf8
TIm Anhalt <tim@anhalt.me>
2022-05-05 09:48:12 +0200

    [VEX-102]: Added data checking before importing

--- Web/import-tool/src/core/index.ts
+++ Web/import-tool/src/core/index.ts
@@ -1,6 +1,7 @@
 import { GameModel } from "../models/GameModel"
 import { doc, setDoc } from "firebase/firestore"; 
 import { auth, db } from "../firebase";
+import { coreError } from "./error";
 
 const NUM_PREVIEW_HISTORY_ENTRIES = 10;
 
@@ -23,6 +24,42 @@ function loadFile (file: File): any
 
 
 
+// Check validity of data
+function checkData (data: any): boolean
+{
+	if (data === null || data === undefined) {
+		coreError("FILE_NULL");
+		return false;
+	}
+
+	if (data.goals === null || data.goals === undefined) {
+		coreError("GOALS_NULL");
+		return false;
+	}
+
+	if (data.streak === null || data.streak === undefined) {
+		coreError("STREAK_NULL");
+		return false;
+	}
+
+	if (data.seasons === null || data.seasons === undefined) {
+		coreError("SEASONS_NULL");
+		return false;
+	}
+
+	if (data.seasons.length <= 0) {
+		coreError("SEASONS_EMPTY")
+		return false;
+	}
+
+	if (data.seasons[0].history === null || data.seasons[0].history === undefined) {
+		coreError("LEGACY_SEASONS");
+		return false;
+	}
+
+	return true;
+} 
+
 function defaultPreview (): any
 {
 	var ret: any = {}
@@ -40,12 +77,7 @@ function parsePreview (data: any): any
 {
 	var ret: any = {}
 
-	if (data === null || data?.seasons === null || data?.seasons?.length <= 0)
-	{
-		alert("Inavlid file")
-		data = null;
-		return defaultPreview();
-	}
+	if(!checkData(data)) return defaultPreview();
 	const season: any = data?.seasons[data?.seasons?.length - 1];
 
 	ret.season_name = season.name as string;
@@ -73,7 +105,6 @@ function parsePreview (data: any): any
 		ret.season_history.push(game)
 	}
 
-	console.log(ret);
 	return ret;
 }
 
@@ -81,11 +112,67 @@ function parsePreview (data: any): any
 const importData = async (data: any) => {
 	const userID = auth.currentUser?.uid.toString() as string;
 
+	var goalgroups = [];
+	var streak = [];
+	var seasons = [];
+
+	if (!checkData(data)) return;
+
+	// Load GoalGroups
+	for (var i = 0; i < data.goals.length; i++) {
+		var goalgroup: any = {};
+		var gg = data.goals[i];
+
+		goalgroup.uuid = gg.uuid;
+		goalgroup.name = gg.name;
+		goalgroup.goals = gg.goals;
+
+		goalgroups.push(goalgroup);
+	}
+
+	// Load Streak
+	streak = data.streak;
+
+	// Load Seasons
+	for (var i = 0; i < data.seasons.length; i++) {
+		var season: any = {};
+		var s = data.seasons[i];
+
+		season.uuid = s.uuid;
+		season.name = s.name;
+		season.endDate = s.endDate;
+		season.activeLevel = s.activeBPLevel;
+		season.activeXP = s.cXP;
+		season.history = []
+
+		// Load Season History
+		for (var j = 0; j < s.history.length; j++) {
+			var entry: any = {};
+			var e = s.history[j]
+
+			entry.uuid = e.uuid;
+			entry.mode = e.gameMode;
+			entry.time = e.time;
+			entry.xp = e.amount;
+			entry.map = e.map;
+			entry.desc = e.description;
+			entry.score = e.score;
+			entry.enemyScore = e.enemyScore;
+			entry.surrenderedWin = e.surrenderedWin;
+			entry.surrenderedLoss = e.surrenderedLoss;
+
+			season.history.push(entry)
+		}
+
+		seasons.push(season);
+	}
+
+	// Push Data to Database
 	await setDoc(doc(db, "users", userID), {
 		userid: userID,
-		goals: data.goals,
-		streak: data.streak,
-		seasons: data.seasons,
+		goalgroups: goalgroups,
+		streak: streak,
+		seasons: seasons,
 	}).then(() => {
 		alert("Successfully imported VexTrack Data to account: " + auth.currentUser?.displayName);
 	});

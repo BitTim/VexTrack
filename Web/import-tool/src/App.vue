ca361a0d92585000db121c239630c5114f636461
TIm Anhalt <tim@anhalt.me>
2022-05-07 00:19:41 +0200

    [VEX-102]: Added tailwind css and started on Design

--- Web/import-tool/src/App.vue
+++ Web/import-tool/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-	<div>
-		<h1>VexTrack Import Tool</h1>
+	<div class="bg-lightBG text-lightFG dark:bg-DarkBG dark:text-DarkFG min-h-screen p-2">
+		<nav class="flex flex-wrap 
+			bg-LightShade dark:bg-DarkShade 
+			items-center justify-start
+			h-16 pl-4 pr-4 p-2
+			shadow-lg rounded-lg">
+			<img class="h-full" src="./assets/logo.png"/>
+			<h1 class="font-bold text-xl pl-4">VexTrack Import Tool</h1>
 
-		<nav>
-			<router-link to="/" v-if="store.user">Home</router-link>
-			<router-link to="/login" v-if="!store.user">Login</router-link>
-			<router-link to="/signup" v-if="!store.user">Sign Up</router-link>
-			<button v-if="store.user" @click="store.signout()">Logout</button>
+			<router-link class="pl-10" to="/" v-if="store.user">Home</router-link>
+			<router-link class="pl-10" to="/login" v-if="!store.user">Login</router-link>
+			<router-link class="pl-10" to="/signup" v-if="!store.user">Sign Up</router-link>
+
+			<div class="ml-auto">
+				<button v-if="store.user" @click="store.signout()">Logout</button>
+			</div>
 		</nav>
 		
 		<router-view />
@@ -29,9 +37,3 @@ export default {
 	}
 }
 </script>
-
-<style>
-nav > * {
-	margin: 1%;
-}
-</style>
---
+++ Web/import-tool/tailwind.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				'LightBG': '#ffffff',
+				'LightShade': '#e5e5e5',
+				'LightFG': '#515151',
+				
+				'DarkBG': '#404040',
+				'DarkShade': '#333333',
+				'DarkFG': '#ffffff',
+
+				'Blue1': '#1684fc',
+				'Blue2': '#6975e8',
+				'Teal1': '#10a780',
+				'Teal2': '#009e97',
+				'Green1': '#0ac903',
+				'Green2': '#00bd5c',
+				'Yellow1': '#efb431',
+				'Yellow2': '#de9321',
+				'Orange1': '#fc8316',
+				'Orange2': '#f7595f',
+				'Red1': '#e2030b',
+				'Red2': '#d40058',
+				'Purple1': '#8a16fc',
+				'Purple2': '#ff00b9',
+
+				'Cyberpunk1': '#1ae699',
+				'Cyberpunk2': '#dd0070',
+				'Cyberpunk3': '#e70efc',
+				'Cyberpunk4': '#31f96f',
+				'Lavender1': '#7a13b1',
+				'Lavender2': '#d086bb',
+				'Aqua1': '#463bac',
+				'Aqua2': '#17fbe5',
+				'Nature1': '#18a358',
+				'Nature2': '#c58c73',
+				'Emerald1': '#2ea783',
+				'Emerald2': '#108321',
+				'Chocolate1': '#89384c',
+				'Chocolare2': '#db6a3a',
+
+				'WinColor1': '#66c2a9',
+				'WinColor2': '#19b2b5',
+				'LossColor1': '#f05c57',
+				'LossColor2': '#e54c7c',
+			}
+		},
+	},
+	plugins: [],
+}
